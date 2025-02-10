d53683693aefec0b97fd0367beb2b11bdd10eaa0
Shelly Wu <yiwenw29@uci.edu>
2025-02-09 21:52:58 -0800

    add following group and service

--- dilidili-dao/src/main/java/com/example/dilidili/dao/UserFollowingDao.java
+++ dilidili-dao/src/main/java/com/example/dilidili/dao/UserFollowingDao.java
@@ -1,4 +1,19 @@
 package com.example.dilidili.dao;
+import com.example.dilidili.domain.UserFollowing;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
 
+@Mapper
 public interface UserFollowingDao {
+
+    Integer deleteUserFollowing(@Param("userId") Long userId, @Param("followingId") Long followingId);
+
+    Integer addUserFollowing(UserFollowing userFollowing);
+
+    List<UserFollowing> getUserFollowings(Long userId);
+
+    List<UserFollowing> getUserFans(Long userId);
+
+    void updateUserFollowings(UserFollowing userFollowing);
 }
